07ba76758cfad929c07d0a86ca07a0ef93b81a0c
Anna Konstantynova <hannakonstantinova@gmail.com>
2024-03-05 09:17:33 +0000

    Add reviews

--- gym-app-landingpage/src/routes/+page.svelte
+++ gym-app-landingpage/src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
     import Hero from "../components/Hero.svelte";
     import Product from "../components/Product.svelte";
+    import Reviews from "../components/Reviews.svelte";
 </script>
 <main class="flex flex-col">
     <Hero/>
     <Product/>
+    <Reviews/>
 </main>
