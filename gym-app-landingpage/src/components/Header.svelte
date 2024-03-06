1e3281c3e0115a1759c6d34a9d0914d27006526d
Anna Konstantynova <hannakonstantinova@gmail.com>
2024-03-06 10:29:31 +0000

    Create a responsive navbar

--- gym-app-landingpage/src/components/Header.svelte
+++ gym-app-landingpage/src/components/Header.svelte
@@ -1,5 +1,5 @@
 <script>
-
+    import { openModal } from "../store";
 </script>
 
 <header class="flex flex-col relative z-20">
@@ -7,11 +7,14 @@
         <a href="/">
         <h1 class="font-semibold"><span class="text-indigo-400">PowerFit</span> Gym</h1>
         </a>
-        <button class="md:hidden grid place-items-center">
+        <button
+            on:click={() => ($openModal = true)}
+            class="md:hidden grid place-items-center"
+        >
             <i class="fa-solid fa-bars"></i>
         </button>
         <nav class="hidden md:flex items-center gap-4 lg:gap-6">
-            <a href="#project" class="duration-200 hover:text-indigo-400 cursor-pointer">Product</a>
+            <a href="#product" class="duration-200 hover:text-indigo-400 cursor-pointer">Product</a>
             <a href="#reviews" class="duration-200 hover:text-indigo-400 cursor-pointer">Reviews</a>
             <a href="#faqs" class="duration-200 hover:text-indigo-400 cursor-pointer">FAQs</a>
             <button class="specialBtn">
